610792197bb86f00454c7eeb51d3e2c720a9e9d9
noondaydream <104835917+thenoondaydream@users.noreply.github.com>
2022-06-21 17:57:04 +0900

    setup

---
+++ database/schema/08_create_stock.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `stock` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `size` varchar(50) NOT NULL,
+  `stock` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
---
+++ database/schema/09_create_sales_count.sql
@@ -0,0 +1,5 @@
+CREATE TABLE `sales_count` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `sales_count` int NOT NULL,
+  PRIMARY KEY (`id`)
+);
