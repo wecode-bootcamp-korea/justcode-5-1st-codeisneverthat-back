9c154edbb9c8711e782f1d588714726d51b8b641
noondaydream <104835917+thenoondaydream@users.noreply.github.com>
2022-06-22 14:42:18 +0900

    Fix: Schema file namings and comma, semicolon added.

--- database/schema/06_create_product_images.sql
+++ database/schema/06_create_product_images.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `product_images` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` int PRIMARY KEY NOT NULL AUTO_INCREMENT,
   `url` varchar(300) NOT NULL,
   `color_id` int NOT NULL,
   FOREIGN KEY fk_product_images_color_id_color_id (`color_id`) REFERENCES `color` (`id`) ON DELETE CASCADE
