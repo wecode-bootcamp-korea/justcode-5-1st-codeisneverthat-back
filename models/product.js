bfcc610107e89e2bc793ae4319cd0b0cbf7b091b
noondaydream <104835917+thenoondaydream@users.noreply.github.com>
2022-06-26 00:39:00 +0900

    Add: collection page API Router 생성 및 엔드포인트 (/collection) 추가 - controllers, models 디렉토리에 collection page 관련 부분 코드 추가 - models에서 readCollectionListByCategory 함수에 해당 쿼리문 추가         - 로 삼항연산자(템플릿 리터럴) 넣어서, 카테고리 아이디가 들어갔을 때에는 id별 상품 리스트를 불러오고, id가 없는경우 all 상품 리스트를 불러올 수 있도록  구현 - controllers에서 readCollectionListByCategoryController 코드 추가 - 제품 상세페이지를 위한 엔드포인트 구현

--- models/product.js
+++ models/product.js
@@ -65,7 +65,47 @@ async function readProductDetails(id) {
   return productDetails;
 }
 
+async function readCollectionListByCategory(id) {
+  const collectionListByCategory = await prisma.$queryRawUnsafe(`
+  SELECT
+      p.id AS productId,
+      p.name AS productName,
+      p.price,
+      cg.id AS categoryId,
+      pa.colorImage,
+      piJA.stockBySize
+  FROM (
+      SELECT pc.product_id, JSON_ARRAYAGG(JSON_OBJECT("id", pc.id, "color", JSON_OBJECT("id", pc.color_id, "color", c.color), "images", pi.images)) AS colorImage
+      FROM product_color pc
+      JOIN color c on pc.color_id = c.id
+      JOIN (
+          SELECT product_color_id AS id, JSON_ARRAYAGG(JSON_OBJECT("id",product_images.id, "url", product_images.url)) AS images
+          FROM product_images WHERE MOD(id,2) = 1
+          GROUP BY product_color_id
+          ) pi on pi.id = pc.id
+      GROUP BY pc.product_id
+      ) pa
+  JOIN products p ON p.id = pa.product_id
+  JOIN category cg ON cg.id = p.category_id
+  JOIN product_color pc2 on p.id = pc2.product_id
+  JOIN color cc ON pc2.color_id = cc.id
+  JOIN product_details pd ON pd.product_color_id = pc2.id
+  JOIN (SELECT pcc.product_id, JSON_ARRAYAGG(JSON_OBJECT("id", pcc.id, "color", JSON_OBJECT("id", pcc.color_id, "color", ccc.color),"size_stock", ss.size_stock)) AS stockBySize
+      FROM product_color pcc
+      JOIN (SELECT product_color_id, JSON_ARRAYAGG(JSON_OBJECT("size", s.size, "stock", product_details.stock)) AS size_stock
+          FROM product_details
+          JOIN size s on product_details.size_id = s.id
+          GROUP BY product_details.product_color_id) ss ON ss.product_color_id = pcc.id
+      JOIN color ccc on ccc.id = pcc.color_id
+      GROUP BY pcc.product_id) piJA on piJA.product_id = p.id
+  GROUP BY pa.product_id
+  ${id ? `HAVING categoryId = ${id}` : ``}
+  `);
+  return collectionListByCategory;
+}
+
 module.exports = {
   readTop20,
   readProductDetails,
+  readCollectionListByCategory,
 };
