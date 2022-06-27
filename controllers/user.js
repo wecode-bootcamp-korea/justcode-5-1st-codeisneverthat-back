8cb8ecb964559198380ca052a8a561ac7e410fef
Sujin Choi <hissujinc@gmail.com>
2022-06-27 20:35:18 +0900

    Fix: controllers>users.js 로그인 success 수정

--- controllers/user.js
+++ controllers/user.js
@@ -15,7 +15,7 @@ async function loginController(req, res) {
   const { email, password } = req.body;
   try {
     const token = await login(email, password);
-    return res.status(201).json({ token });
+    return res.status(201).json({ token, message: 'SUCCESS' });
   } catch (err) {
     res.status(500).json({ message: '로그인 실패' });
     return;
