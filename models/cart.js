de04abd6239219e13ec9a4ae06bca9d952e2e59b
Bokyung Kim <kbkllmlk17@gmail.com>
2022-06-27 13:02:54 +0900

    cart api 두번 출력되는 문제 고쳤습니다

--- models/cart.js
+++ models/cart.js
@@ -1,10 +1,9 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-async function getCartById(user_id) {
 
-
-  const  cart  = prisma.$queryRaw`
+    async function getCartById(user_id) {
+        const  cart  = prisma.$queryRaw`
      SELECT 
          products.name,
          product_images.url,
@@ -12,13 +11,15 @@ async function getCartById(user_id) {
          cart.quantity,
          color.color,
          size.size
-     FROM users LEFT JOIN cart on ${user_id} = cart.user_id
+     FROM users INNER JOIN cart on ${user_id} = cart.user_id
              LEFT JOIN product_details on cart.product_details_id = product_details.id 
              LEFT JOIN size on product_details.size_id = size.id 
             LEFT JOIN product_color on product_details.product_color_id = product_color.id
-             LEFT JOIN product_images on product_color.id = product_images.product_color_id
-             LEFT JOIN color on product_color.color_id = color.id
+            
+            LEFT JOIN color on product_color.color_id = color.id
             LEFT JOIN products on product_color.product_id = products.id
+            LEFT JOIN product_images on product_color.id = product_images.product_color_id
+    WHERE MOD(product_images.id,2) = 0
      `;
 
    return cart;
@@ -31,7 +32,6 @@ async function addItem(addItemDto) {
         INSERT INTO cart (user_id, product_details_id, quantity)
         VALUES (${user_id}, ${product_details_id}, ${quantity});
     `;
-
 
 
 }
--- services/cart.js
+++ services/cart.js
@@ -1,10 +1,5 @@
 const {addItem} = require ('../models/cart')
 
-// async function getCartItem(user_id) {
-//     const cart = await getCartById(user_id);
-//     return cart;
-
-// }
 
 async function addCartItem(user_id, product_details_id, quantity) {
     const addItemDto = {
