c56e5f9ea76afd5887ca32beaab61014bd3f3813
Sujin Choi <hissujinc@gmail.com>
2022-07-06 23:49:51 +0900

    Fix: middlewares changing folder name, token status check

--- middlewares/validateToken.js
+++ middlewares/validateToken.js
@@ -3,8 +3,10 @@ const jwt = require('jsonwebtoken');
 const validateToken = async (req, res, next) => {
   try {
     const token = req.header('Authorization');
-    const user_id = jwt.verify(token, process.env.SECRET_KEY);
-    req.userId = user_id.id;
+    console.log(token);
+    const user = jwt.verify(token, process.env.SECRET_KEY);
+    console.log(user);
+    req.userId = user.id;
     next();
   } catch (err) {
     next(err);
